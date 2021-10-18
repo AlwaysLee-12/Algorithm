5e12a7c8d03b5d0ae59aa9f0c184a522d7322282
AlwaysLee <54054460+AlwaysLee-12@users.noreply.github.com>
2021-10-18 18:15:18 +0900

    Update 전화번호목록.py

--- 프로그래머스/전화번호목록.py
+++ 프로그래머스/전화번호목록.py
@@ -2,9 +2,6 @@ def solution(phone_book):
 	phone_book.sort()
 	answer = True
 	b= dict()
-
-	for i in phone_book:
-		b[i]= i[:1]
 		
 	for i in range(len(phone_book)-1):
 			if(phone_book[i]==phone_book[i+1][:len(phone_book[i])]):
