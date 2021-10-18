ffa1c9c81136a55bdd97bb8464776b92ba25ed4e
AlwaysLee <54054460+AlwaysLee-12@users.noreply.github.com>
2021-10-18 15:11:56 +0900

    Update 완주하지못한선수.py

--- 프로그래머스/완주하지못한선수.py
+++ 프로그래머스/완주하지못한선수.py
@@ -1,5 +1,9 @@
+# zip()은 배열 간 같은 인덱스끼리 묶어준다. 배열의 길이가 다르면 한쪽이 남게된다.
 def solution(participant, completion):
-    for i in completion:
-        participant.pop(participant.index(i))
-    answer=participant[0]
-    return answer
+    participant.sort()
+    completion.sort()
+
+    for a,b in zip(participant,completion):
+        if a!=b:
+            return a
+    return participant.pop()
