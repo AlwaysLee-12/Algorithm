0f0fb8f1ebe4443ee16b968d6409549b7abd52ba
AlwaysLee <54054460+AlwaysLee-12@users.noreply.github.com>
2021-11-08 11:24:07 +0900

    Create 소수찾기.py

---
+++ 프로그래머스/소수찾기.py
@@ -0,0 +1,27 @@
+import itertools
+
+def solution(numbers):
+	answer = 0
+	numbers= list(numbers)
+	permu_list= []
+
+	numbers.sort()
+
+	for i in range(1,len(numbers)+1):
+		permu_list.extend(map(int,list(map(''.join, itertools.permutations(numbers,i)))))
+
+	permu_list= list(set(permu_list))
+
+	for i in permu_list:
+		if i>=2:
+			check= True
+
+			for j in range(2,int(i**0.5)+1):
+				if i%j==0:
+					check= False
+					break
+			if check:
+		
+				answer+=1
+
+	return answer
