4da9252bcdf995a4b65eb4de56dffcba57bfca3b
AlwaysLee <54054460+AlwaysLee-12@users.noreply.github.com>
2022-01-26 13:04:29 +0900

    Update 채널톡 3번.py

--- 프로그래머스/채널톡 3번.py
+++ 프로그래머스/채널톡 3번.py
@@ -29,3 +29,27 @@ def solution(s: str)-> int:
 					else:
 						dp_table[i][length] = (dp_table[i - len(str(current_num))][length] + length)
 				answer = max(answer, dp_table[i][length])
+
+	# remove duplication code
+	def solution(s: str)-> int:
+	answer = 0
+	dp_table = [[0] * 6 for _ in range(len(s))] 
+
+	for i in range(1, len(s)):
+		for length in range(1,6):
+			if i - length + 1 < 0:
+				continue 
+
+			current_num = int(s[i - length + 1 : i + 1])
+			target_num = current_num - 1
+			if i - length - len(str(target_num)) + 1 < 0 or (s[i - length + 1 : i + 1][0] == '0'):
+				continue
+
+			if int(s[i - length - len(str(target_num)) + 1 : i - length + 1]) == target_num:
+				if dp_table[i - len(str(current_num))][length - (len(str(current_num)) - len(str(target_num)))] == 0:
+					dp_table[i][length] = (length * 2 - (len(str(current_num)) - len(str(target_num)))) 
+				else:
+					dp_table[i][length] = (dp_table[i - len(str(current_num))][length - (len(str(current_num)) - len(str(target_num)))] + length)
+				answer = max(answer, dp_table[i][length])
+
+	return answer
