3fa01789842ee1cd6cc9ca5d273c72914b5d186e
AlwaysLee <54054460+AlwaysLee-12@users.noreply.github.com>
2023-02-09 23:52:58 +0900

    Update 전광판.java

--- softeer/전광판.java
+++ softeer/전광판.java
@@ -111,3 +111,91 @@ public class Main
  * 3-2. A의 자리수가 더 크다면, B 자리수에 맞춰 3-1 수행 후 마지막 A의 스위치 상태 배열에서 켜져 있는 스위치 개수 카운트
  * 3-3. 3-2의 과정을 B의 기준에서 수행
 */
+
+//개선된 버전(자릿수 크기를 맞춘 후 비교)
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{
+    static Queue<Integer[]> aQ = new LinkedList<>();
+    static Queue<Integer[]> bQ = new LinkedList<>();
+
+    static int compare(Integer[] aList, Integer[] bList){
+        int result = 0;
+
+        for(int i = 0; i < aList.length; i++){
+            if(aList[i] != bList[i]){
+                result += 1;
+            }
+        }
+
+        return result;
+    }
+
+    static void insertToQueue(Queue<Integer[]> q, Integer[][] eachNumSwitchStateArr, List<Integer> valList){
+        for(Integer val : valList){
+            q.offer(eachNumSwitchStateArr[val]);
+        }
+    }
+
+    static List<Integer> getEachDigitVal(int num){
+        List<Integer> arr = new ArrayList<>();
+
+        while(num > 0){
+            int val = num % 10;
+
+            arr.add(val);
+            num /= 10;
+        }
+
+        return arr;
+    }
+
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Integer[][] eachNumSwitchStateArr = {{1,1,1,0,1,1,1}, {0,0,1,0,0,0,1}, {0,1,1,1,1,1,0}, {0,1,1,1,0,1,1}, {1,0,1,1,0,0,1}, {1,1,0,1,0,1,1}, {1,1,0,1,1,1,1}, {1,1,1,0,0,0,1}, {1,1,1,1,1,1,1}, {1,1,1,1,0,1,1}, {0,0,0,0,0,0,0}};
+    
+        int testCaseCount = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < testCaseCount; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int switchPushCount = 0;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            List<Integer> aValList = getEachDigitVal(a);
+            List<Integer> bValList = getEachDigitVal(b);
+
+            insertToQueue(aQ, eachNumSwitchStateArr, aValList);
+            insertToQueue(bQ, eachNumSwitchStateArr, bValList);
+
+            //자릿수 
+            int aQSize = aQ.size();
+            int bQSize = bQ.size();
+
+            if(aQ.size() > bQ.size()){
+                for(int j = 0; j < aQSize - bQSize; j++){
+                    bQ.offer(eachNumSwitchStateArr[10]);
+                }
+            }else if(bQ.size() > aQ.size()){
+                for(int j = 0; j < bQSize - aQSize; j++){
+                    aQ.offer(eachNumSwitchStateArr[10]);
+                }
+            }
+
+            while(aQ.size() > 0){
+                switchPushCount += compare(aQ.poll(), bQ.poll());
+            }
+
+            bw.write(String.valueOf(switchPushCount) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
