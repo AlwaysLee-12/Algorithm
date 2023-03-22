96f2b9a361480a0816a51790079311fbbf45c22f
AlwaysLee <54054460+AlwaysLee-12@users.noreply.github.com>
2023-03-22 14:07:11 +0900

    Create 11382.java

---
+++ 백준/11382.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        
+        bw.write(String.valueOf(A + B + C));
+        bw.flush();
+        bw.close();
+    }
+}
