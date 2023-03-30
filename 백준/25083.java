0a4e4152c7b421f7cf690fade730f72b1602b937
AlwaysLee <54054460+AlwaysLee-12@users.noreply.github.com>
2023-03-30 10:50:05 +0900

    Create 25083.java

---
+++ 백준/25083.java
@@ -0,0 +1,12 @@
+//이스케이프 문자를 이용한 출력 문제
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r'");           
+        System.out.println("   `~\\/");   
+        System.out.println("      |");   
+        System.out.println("      |");
+    }
+}
